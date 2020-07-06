9561f694d740846a6b5408b3dee0f8ebb721f7a1
fake364 <serebreanu.v@gmail.com>
2020-07-06 11:11:50 +0300

    added more tests

--- __test__/mainpage.test.js
+++ __test__/mainpage.test.js
@@ -4,18 +4,28 @@ import {Provider} from "react-redux";
 import React from "react";
 import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
-import initState from "./__mocks__/initstateforlogin.state"
 import {Router} from "react-router-dom";
 import App from "../src/client/components/App";
 import {createMemoryHistory} from "history";
+import initState from "./__mocks__/user";
 
-describe("Check successfull attempt of register", () => {
-    let elementTree;
-    const mockStore = configureStore([thunk]);
-    let store = mockStore(initState);
+describe("Main page", () => {
 
+    it("should render correctly if im not authed", () => {
+        let elementTree;
+        const mockStore = configureStore([thunk]);
+        let store = mockStore(initState.userNotFoundAndNotAuthed);
+        const history = createMemoryHistory();
+        history.push('/');
+        elementTree = renderer.create(<Provider
+            store={store}><Router history={history}><App/></Router></Provider>);
+        expect(elementTree.toJSON()).toMatchSnapshot();
+    });
 
-    it("i'm on the main page", () => {
+    it("should render correctly if im authed", () => {
+        let elementTree;
+        const mockStore = configureStore([thunk]);
+        let store = mockStore(initState.userNotFoundAndAuthed);
         const history = createMemoryHistory();
         history.push('/');
         elementTree = renderer.create(<Provider
